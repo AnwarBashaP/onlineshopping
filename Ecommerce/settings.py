5617fcc927ea7d8dc1d4a970d21095ad51cd5611
anwar-codoid <anwar.codoid@gmail.com>
2020-08-09 12:51:52 +0530

    make it better3

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated', ),
 
 }
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -151,9 +151,10 @@ DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
 
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
